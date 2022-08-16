ec5d1bb33fb38dadbcf6ea0219029e801bbce9f3
Marsel151205 <mambetalievmarsel2289@gmail.com>
2022-08-16 17:15:33 +0600

    f

--- app/src/main/java/com/example/todo/ui/fragments/OnBoardingFragment.kt
+++ app/src/main/java/com/example/todo/ui/fragments/OnBoardingFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.fragment.findNavController
 import com.example.todo.R
 import com.example.todo.databinding.FragmentOnBoardingBinding
@@ -36,7 +37,8 @@ class OnBoardingFragment : Fragment(), OnItemClicker {
     }
 
     override fun onClick() {
-        findNavController().navigate(R.id.action_onBoardingFragment_to_homeFragment)
+        findNavController().navigate(R.id.clearBackStack)
+
     }
 
     override fun onClickNext() {
